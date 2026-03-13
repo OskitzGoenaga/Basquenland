b3000985bbb02d5e2b3c20cf18e2c2e7c9afc94a
Djessy <djessyvd@icloud.com>
2026-03-13 10:44:57 +0100

    ive made the full html
    this is the full html of the index page im now going to do the styling and js

--- index.php
+++ index.php
@@ -43,10 +43,65 @@
                 </div>
             </div>
         </div>
-        
+        <div class="promotions-img">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+        </div>
+        <div class="tilburg">
+            <div>
+                <h4>LET’S see the information about Tilburg</h4>
+                <h1>Ready to see tilburg Let’s Get Started!</h1>
+            </div>
+            <div>
+                <p> Here you can find all the information
+                    and fun facts about Tilburg. Discover
+                    its history, unique places, culture,
+                    events, and interesting details that
+                    make the city special and enjoyable
+                    to explore.</p>
+                <button>see the info</button>
+            </div>
+        </div>
+        <div class="basqueland">
+            <div>
+                <h4>LET’S see the information about Tilburg</h4>
+                <h1>What is there to see in Basqueland?</h1>
+            </div>
+            <div>
+                <p> Here you can find all the information
+                    and fun facts about the Basque Country.
+                    Discover its rich history, unique culture,
+                    beautiful landscapes, traditions,
+                    and interesting facts that make this
+                    region special and exciting to explore.</p>
+                <button>see the info</button>
+            </div>
+        </div>
+        <div class="quiz-promo">
+            <h1>think you know enough Take the quiz</h1>
+            <button>The quiz</button>
+        </div>
     </main>
     <footer>
-
+        <div>
+            <h1>basqueland x Tillie</h1>
+            <p>Our exchange project between Tilburg and Bakuland helped students share cultures, work together, learn
+                new ideas, and build friendships while exploring schools, traditions, and daily life together.</p>
+            <h3>&copy; 2026</h3>
+        </div>
+        <div>
+            <div>
+                <h3>pages</h3>
+                <h4>tilburg</h4>
+                <h4>basqueland</h4>
+                <h4>quiz</h4>
+                <h4>about us</h4>
+            </div>
+        </div>
     </footer>
 </body>
 

d45f8e98978861880cf99d9b3c59bb79722dedb9
Djessy <djessyvd@icloud.com>
2026-03-13 10:27:24 +0100

    head

--- index.php
+++ index.php
@@ -3,9 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Basqueland x Tillie project</title>
 </head>
 <body>
-    <p>First WEB</p>
+    <header>
+        <h1>Basqueland x Tillie project</h1>
+        <div>
+            <a href="">basqueland</a>
+            <a href="">Tilburg</a>
+            <a href="">About us</a>
+        </div>
+        <div>
+            <button>music</button>
+            <button>quiz</button>
+        </div>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
 </body>
 </html>
